96610d4d3df967645b1fb809a73e59786ff033e2
mzecic <matejzecic@gmail.com>
2023-01-21 09:12:49 -0500

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "/Users/matejzecic/code/react-comp/src/stories/Requirements.stories.jsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
